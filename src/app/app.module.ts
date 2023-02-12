96680580b928fd6f9072a36070b6bde1d3d9bae3
Lmc95 <95carranzalm@gmail.com>
2023-02-12 02:19:29 -0300

    img portada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExpEduComponent } from './componentes/exp-edu/exp-edu.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { PortadaComponent } from './componentes/portada/portada.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    ExpEduComponent,
+    FooterComponent,
+    HeaderComponent,
+    PortadaComponent,
+    ProyectosComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
